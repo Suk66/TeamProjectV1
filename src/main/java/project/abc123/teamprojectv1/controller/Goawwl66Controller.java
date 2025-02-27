7f37e13d5333458513a5198e5c9cbea8f9f7a34e
Suk66 <goawwl66@gmail.com>
2025-02-27 15:25:28 +0900

    현석에 팀장님에게 푸쉬

---
+++ src/main/java/project/abc123/teamprojectv1/controller/Goawwl66Controller.java
@@ -0,0 +1,13 @@
+package project.abc123.teamprojectv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class Goawwl66Controller {
+
+    @GetMapping("/goawwl66")
+    public String index() {
+
+        return "views/howau";
+    }
+}
